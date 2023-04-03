f4fa69dcc6a09a9ff294dd3ab8e96115c0739af0
Alice <xinranjiang2015@yahoo.com>
2023-04-02 21:18:12 -0400

    friend req front

--- frontend/src/components/FriendRequests.js
+++ frontend/src/components/FriendRequests.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Form from 'react-validation/build/form';
 import Input from 'react-validation/build/input';
+import '../css/Friend.css';
+
 const API_URL = 'http://localhost:3000/';
-
-
 export default function FriendRequests() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <div>FriendRequests</div>
-  )
+    <div className="friend-request-window">
+      <div className="search-container">
+        <h2>Add Friends</h2>
+        <Form>
+          <label htmlFor="username"></label>
+          <Input
+            type="text"
+            placeholder="Search by username"
+            value={searchTerm}
+            onChange={handleSearchInputChange}
+          />
+        </Form>
+      </div>
+      <h2>Friend Requests</h2>
+      <ul className="friend-list">
+        <li className="friend-request">
+          <div className="friend-info">
+            <img src="https://example.com/avatar1.jpg" alt="Avatar" />
+            <div className="friend-name">John Doe</div>
+          </div>
+          <div className="friend-actions">
+            <button className="view-friend-btn">View Profile</button>
+            <button className="add-friend-btn">Add Friend</button>
+            <button className="view-request-btn">Reject friend request</button>
+          </div>
+        </li>
+        <li className="friend-request">
+          <div className="friend-info">
+            <img src="https://example.com/avatar2.jpg" alt="Avatar" />
+            <div className="friend-name">Jane Smith</div>
+          </div>
+          <div className="friend-actions">
+            <button className="view-friend-btn">View Profile</button>
+            <button className="add-friend-btn">Add Friend</button>
+            <button className="view-request-btn">Reject friend request</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import AuthService from '../services/auth.service';
+import FriendRequests from './FriendRequests';
 import Posts from './Posts';
 import Reviews from './Reviews';
 
@@ -18,11 +19,12 @@ const Profile = () => {
           <strong>{currentUser.username}</strong> Profile
         </h3>
       </header>
-
       {/* items of interest */}
       <Posts></Posts>
       {/* reviews and ratings */}
       <Reviews></Reviews>
+      {/* friends and friend requests  */}
+      <FriendRequests></FriendRequests>
     </div>
   );
 };
